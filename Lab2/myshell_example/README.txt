f4b13aa60faa6cd0d93485be5d0864349df1be99
Carlos Fabregas <carlosfabregas@Carlos-MacBook-Pro.local>
2018-03-02 17:09:07 -0500

    lab2

--- Lab2/myshell_example/README.txt
+++ Lab2/myshell_example/README.txt
@@ -1,2 +1,13 @@
+---------------------
+LAB 2 - Project shell
+---------------------
+Available Commands
+---------------------
 help - Display the user manual using the more filter.
+clr - Clear all text in the terminal
+environ - list all environmental strings
+dir  - Display current project directory
+pause - Pause current session
 quit - Quit the shell.
+---------------------
+
--- Lab2/myshell_example/myshell.c
+++ Lab2/myshell_example/myshell.c
@@ -1,10 +1,8 @@
 /*
  * MyShell Project for SOFE 3950U / CSCI 3020U: Operating Systems
- *
- * Copyright (C) 2017, <GROUP MEMBERS>
- * All rights reserved.
- * 
- */
+ * Jonas Albaira 100555164
+ * Carlos Fabregas 100572307
+  */
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -13,6 +11,8 @@
 #include <string.h>
 #include "utility.h"
 #include "myshell.h"
+
+extern char **environ;
 
 // Put macros or constants here using #define
 #define BUFFER_LEN 256
@@ -30,44 +30,59 @@ int main(int argc, char *argv[])
     getcwd(arg, BUFFER_LEN);
     // Parse the commands provided using argc and argv
     printf("%s/myshell>",arg);
+    
     // Perform an infinite loop getting command input from users
     while (fgets(buffer, BUFFER_LEN, stdin) != NULL)
-    {
+    {   
+        
         // Perform string tokenization to get the command and argument
             char *dir;
             memcpy(command, buffer, BUFFER_LEN);
             strtok_r(command, "\n", &dir); //removes \n in the command array
             strcpy(buffer,dir);
             strtok(buffer, "\n");
-        // Check the command and execute the operations for each command
-
 
         // cd command -- change the current directory
         if (strcmp(command, "cd") == 0)
         {
-            memcpy(arg, change_dir(buffer, arg), BUFFER_LEN);
-            // your code here
+
+            printf("%s", "Command not implemented. \nUse 'help' to view available commands. \n");
         }
 
         //clear command
         else if(strcmp(command, "clr") == 0)
-        {
-    
+        {       
+            
+            const char *CLEAR_SCREEN_ANSI = "\e[1;1H\e[2J";
+            write(STDOUT_FILENO, CLEAR_SCREEN_ANSI, 12);
+
         }
         //dir command - display current directory
         else if(strcmp(command, "dir") == 0)
         {
-    
+                char cwd[1024];
+                if (getcwd(cwd, sizeof(cwd)) != NULL)
+                    fprintf(stdout, "Current working dir: %s\n", cwd);
+                else
+                    perror("getcwd() error");
+                return 0;
         }
         //environ command - list all environmental strings
         else if(strcmp(command, "environ") == 0)
         {
-    
+                int i = 1;
+                char *s = *environ;
+
+                for (; s; i++) {
+
+                    printf("%s\n", s);
+                    s = *(environ+i);
+                }
         }
         //echo command - displays comment
         else if(strcmp(command, "echo") == 0)
         {
-    
+            printf("%s", "Command not implemented. \nUse 'help' to view available commands. \n");
         }
         //help command - 
         else if(strcmp(command, "help") == 0)
@@ -85,7 +100,7 @@ int main(int argc, char *argv[])
         //
         else if(strcmp(command, "pause") == 0)
         {
-    
+            sleep(3);
         }
         // quit command -- exit the shell
         else if (strcmp(command, "quit") == 0)
